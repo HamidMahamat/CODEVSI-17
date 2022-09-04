955353720394d7fa4447cee128104ab170ba4330
SAMA-MOLA <55284357+SAMA-MOLA@users.noreply.github.com>
2022-09-04 12:06:37 +0200

    Mise à jour de l'action bouton Generate
    On selectionne pour l'exemple une image en fonction des  04 premiers paramètres (La somme des entrées modulo le nombre d'image)

--- app.py
+++ app.py
@@ -1,63 +1,65 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk, Image
- 
 
+view = ""
+panel =None
+window = None
 
+views= [ "dancing-spider.jpg","dancing-spider-sobel_avec_seuil_270.png","Pitch-Roll-and-Yaw.jpg","im1.jpg","im2.png"]
+
+index = 0
+view=".essai/"+views[index%len(views)]
+
+#transversal_permittiviy = None
+longitudinal_permittiviy= None
+diameter= None
+azimuthal_index= None
+#k_max_var = tk.StringVar()
 
 def create_main_window():
+    global view,panel,window,transversal_permittiviy,longitudinal_permittiviy,diameter,azimuthal_index,k_max_var
     window=tk.Tk()
     window.title("Dispersion Diagram Displayer")
+    transversal_permittiviy = tk.StringVar()
+    longitudinal_permittiviy= tk.StringVar()
+    diameter= tk.StringVar()
+    azimuthal_index= tk.StringVar()
+    k_max_var = tk.StringVar()
 
     #window.geometry("1080x720")
-    window.iconbitmap("logo.ico")
+    #window.iconbitmap("logo.ico")
     window.config(background='#33A18B')
 
     label_title=tk.Label(window, text="Propagation modes displayer", font=("Helvetica", 20))
 
     # layout on the root window
     window.columnconfigure(0, weight=1)
-    window.columnconfigure(1, weight=5)
+    window.columnconfigure(1, weight=1)
 
     input_frame = create_param_frame(window)
     input_frame.grid(column=0, row=0)
 
-    img = ImageTk.PhotoImage(Image.open('dancing-spider.jpg'))
-    panel = tk.Label(window, image = img)
-    panel.grid(column=1, row=0,columnspan=1,rowspan=1)
+    #img = ImageTk.PhotoImage(file= view)
+    #panel = tk.Label(window, image = img)
+    #panel.grid(column=1, row=0,columnspan=1,rowspan=1)
+
 
     button_frame = create_button_frame(window)
-    button_frame.grid(column=1, row=1)
+    button_frame.grid(column=1, row=0)
     
-    window.resizable(1, 1)
+    window.resizable(0, 0)
     window.mainloop()
     
-#def image_render():
-    
-    
 
-def create_view_frame(container):
-    frame = ttk.Frame(container)
-
-    img = ImageTk.PhotoImage(Image.open('dancing-spider.jpg'))
-    panel = tk.Label(frame, image = img)
-    return frame
+    
 
 def create_button_frame(container):
     frame = ttk.Frame(container)
     
-    frame.columnconfigure(0, weight=1)
-    frame.columnconfigure(1, weight=1)
     
-    frame2 = ttk.Frame(frame)
-    frame2.columnconfigure(0, weight=1)
-    frame2.columnconfigure(1, weight=1)
     
-    ttk.Label(frame2, text="k_max:").grid(column=0, row=0)
-    ttk.Entry(frame2).grid(column=1, row=0, sticky=tk.E)
-    frame2.grid(column=1,row=0)
-    
-    ttk.Button(frame, text='Generate').grid(column=0, row=1)
+    ttk.Button(frame, text='Generate',command=image_render).grid(column=1, row=0)
     
     ttk.Button(frame, text='Save').grid(column=1, row=1)
     
@@ -67,7 +69,7 @@ def create_button_frame(container):
     return frame
     
     
-
+    
         
 def create_param_frame(container):
     frame = ttk.Frame(container)
@@ -76,25 +78,62 @@ def create_param_frame(container):
     frame.columnconfigure(1, weight=1)
 
     ttk.Label(frame, text="transversal permittiviy "+u'\u03B5'+u'\u209C'+" :").grid(column=0, row=0)
-    ttk.Entry(frame).grid(column=1, row=0, sticky=tk.E)
+    ttk.Entry(frame,textvariable=transversal_permittiviy).grid(column=1, row=0, sticky=tk.E)
 
     ttk.Label(frame, text="longitudinal permittiviy "+u'\u03B5'+u'\u2097'+" :").grid(column=0, row=1)
-    ttk.Entry(frame).grid(column=1, row=1, sticky=tk.E)
+    ttk.Entry(frame,textvariable=longitudinal_permittiviy).grid(column=1, row=1, sticky=tk.E)
 
     ttk.Label(frame, text="diameter d :").grid(column=0, row=2)
-    ttk.Entry(frame).grid(column=1, row=2, sticky=tk.E)
+    ttk.Entry(frame,textvariable=diameter).grid(column=1, row=2, sticky=tk.E)
 
     ttk.Label(frame, text="azimuthal index n :").grid(column=0, row=3)
-    ttk.Entry(frame).grid(column=1, row=3, sticky=tk.E)
-
+    ttk.Entry(frame,textvariable=azimuthal_index).grid(column=1, row=3, sticky=tk.E)
+    
+    ttk.Label(frame, text="k_max:").grid(column=0, row=4)
+    ttk.Entry(frame,textvariable = k_max_var).grid(column=1, row=4, sticky=tk.E)
 
     for widget in frame.winfo_children():
         widget.grid(padx=5, pady=5)
     return frame
 
+def image_render():
+    global view,index,panel,window,transversal_permittiviy,longitudinal_permittiviy,diameter,azimuthal_index,k_max_var
+    v1,v2,v3,v4,v5 = None,None,None,None,None
+    index =-1
+    #print("k_max = {}".format(k_max_var.get()))
+    try : #on s'assure de récuperer les bonnes valeurs
+        #print(v1,v2,v3,v4,v5)
+        v1= float(transversal_permittiviy.get())
+        v2= float(longitudinal_permittiviy.get())
+        v3=float(diameter.get())
+        v4= float(azimuthal_index.get())
+        v5 = float(k_max_var.get())
+        index = int(v1+v2+v3+v4)
+        print(v1,v2,v3,v4,v5)
+        #print("k_max = {}".format(v5))
+    except:
+        print("les paramètres sont vides ou format incorrect")
+        print(v1,v2,v3,v4,v5)
+        
+    if index ==-1 :
+        return
+    
+    view = ".essai/"+views[index%len(views)]
+    result = tk.Toplevel(window) 
+    img = ImageTk.PhotoImage(file= view)
+    graph = tk.Label(result, image = img)
+    menubar = tk.Menu(result)  
+    file = tk.Menu(menubar, tearoff=0)  
+    file.add_command(label="Save")   
 
+    file.add_separator()
 
+    file.add_command(label="Exit", command=result.quit)  
+    menubar.add_cascade(label="Fichier", menu=file)  
+    
+    graph.pack()
+    result.config(menu=menubar)  
+    result.mainloop()  
 
 if __name__ =="__main__":
 	create_main_window()
-	
