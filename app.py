b01ae4955e055b3fa84b31188c08e049751f6694
Hamid <khalapoum71@gmail.com>
2022-11-02 03:08:05 +0100

    last updates with maximal frequency

--- app.py
+++ app.py
@@ -1,31 +1,29 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk, Image
+from plotter import main
 
 view = ""
 
-views= [ "dancing-spider.jpg","dancing-spider-sobel_avec_seuil_270.png","Pitch-Roll-and-Yaw.jpg","im1.jpg","im2.png"]
-
 index = 0
-view=+views[index%len(views)]
-
 
 
 def create_main_window():
-    global view,panel,window,transversal_permittiviy,longitudinal_permittiviy,diameter,azimuthal_index,k_max_var
-    window=tk.Tk()
+    global view, panel, window, transversal_permittiviy, longitudinal_permittiviy, diameter, azimuthal_index, k_max_var,list_te_tm_var
+    window = tk.Tk()
     window.title("Dispersion Diagram Displayer")
     transversal_permittiviy = tk.StringVar()
-    longitudinal_permittiviy= tk.StringVar()
-    diameter= tk.StringVar()
-    azimuthal_index= tk.StringVar()
+    longitudinal_permittiviy = tk.StringVar()
+    diameter = tk.StringVar()
+    azimuthal_index = tk.StringVar()
     k_max_var = tk.StringVar()
+    list_te_tm_var = tk.StringVar()
 
     #window.geometry("1080x720")
-    #window.iconbitmap("logo.ico")
+    window.iconbitmap("logo.ico")
     window.config(background='#33A18B')
 
-    label_title=tk.Label(window, text="Propagation modes displayer", font=("Helvetica", 20))
+    label_title = tk.Label(window, text="Propagation modes displayer", font=("Helvetica", 30))
 
     # layout on the root window
     window.columnconfigure(0, weight=1)
@@ -34,99 +32,106 @@ def create_main_window():
     input_frame = create_param_frame(window)
     input_frame.grid(column=0, row=0)
 
-    #img = ImageTk.PhotoImage(file= view)
-    #panel = tk.Label(window, image = img)
-    #panel.grid(column=1, row=0,columnspan=1,rowspan=1)
-
+    # img = ImageTk.PhotoImage(file= view)
+    # panel = tk.Label(window, image = img)
+    # panel.grid(column=1, row=0,columnspan=1,rowspan=1)
 
     button_frame = create_button_frame(window)
     button_frame.grid(column=1, row=0)
-    
+
     window.resizable(0, 0)
     window.mainloop()
-    
 
-    
 
 def create_button_frame(container):
-    frame = ttk.Frame(container)
-    
-    
-    
-    ttk.Button(frame, text='Generate',command=image_render).grid(column=1, row=0)
-    
+    s = ttk.Style()
+    s.configure('My.TFrame', background='#33A18B')
+    frame = ttk.Frame(container,style='My.TFrame')
+
+
+    ttk.Button(frame, text='Generate', command=image_render).grid(column=1, row=0)
+
     ttk.Button(frame, text='Save').grid(column=1, row=1)
-    
+
     for widget in frame.winfo_children():
         widget.grid(padx=0, pady=3)
 
     return frame
-    
-    
-    
-        
+
+
 def create_param_frame(container):
-    frame = ttk.Frame(container)
+    s = ttk.Style()
+    s.configure('My.TFrame', background='#33A18B')
+    frame = ttk.Frame(container,style='My.TFrame')
+
 
     frame.columnconfigure(0, weight=1)
     frame.columnconfigure(1, weight=1)
 
-    ttk.Label(frame, text="transversal permittiviy "+u'\u03B5'+u'\u209C'+" :").grid(column=0, row=0)
-    ttk.Entry(frame,textvariable=transversal_permittiviy).grid(column=1, row=0, sticky=tk.E)
+    ttk.Label(frame, text="Transversal permittivity " + u'\u03B5' + u'\u209C' + " :",background='#33A18B', foreground='white',font=("Palatino", 18)).grid(column=0, row=0)
+    ttk.Entry(frame, textvariable=transversal_permittiviy).grid(column=1, row=0, sticky=tk.E)
 
-    ttk.Label(frame, text="longitudinal permittiviy "+u'\u03B5'+u'\u2097'+" :").grid(column=0, row=1)
-    ttk.Entry(frame,textvariable=longitudinal_permittiviy).grid(column=1, row=1, sticky=tk.E)
+    ttk.Label(frame, text="Longitudinal permittivity " + u'\u03B5' + u'\u2097' + " :",background='#33A18B', foreground='white',font=("Palatino", 18)).grid(column=0, row=1)
+    ttk.Entry(frame, textvariable=longitudinal_permittiviy).grid(column=1, row=1, sticky=tk.E)
 
-    ttk.Label(frame, text="diameter d :").grid(column=0, row=2)
-    ttk.Entry(frame,textvariable=diameter).grid(column=1, row=2, sticky=tk.E)
+    ttk.Label(frame, text="Diameter d (m) :",background='#33A18B', foreground='white',font=("Palatino", 18)).grid(column=0, row=2)
+    ttk.Entry(frame, textvariable=diameter).grid(column=1, row=2, sticky=tk.E)
 
-    ttk.Label(frame, text="azimuthal index n :").grid(column=0, row=3)
-    ttk.Entry(frame,textvariable=azimuthal_index).grid(column=1, row=3, sticky=tk.E)
-    
-    ttk.Label(frame, text="k_max:").grid(column=0, row=4)
-    ttk.Entry(frame,textvariable = k_max_var).grid(column=1, row=4, sticky=tk.E)
+    ttk.Label(frame, text="Azimuthal index n :",background='#33A18B', foreground='white',font=("Palatino", 18)).grid(column=0, row=3)
+    ttk.Entry(frame, textvariable=azimuthal_index).grid(column=1, row=3, sticky=tk.E)
+
+    ttk.Label(frame, text="Maximal frequency (GHz) :",background='#33A18B', foreground='white',font=("Palatino", 18)).grid(column=0, row=4)
+    ttk.Entry(frame, textvariable=k_max_var).grid(column=1, row=4, sticky=tk.E)
+
+    ttk.Label(frame, text="index mode to plot",background='#33A18B', foreground='white',font=("Palatino", 18)).grid(column=0, row=5)
+    ttk.Entry(frame, textvariable=list_te_tm_var).grid(column=1, row=5, sticky=tk.E)
+
 
     for widget in frame.winfo_children():
         widget.grid(padx=5, pady=5)
     return frame
 
+
 def image_render():
-    global view,index,panel,window,transversal_permittiviy,longitudinal_permittiviy,diameter,azimuthal_index,k_max_var
-    v1,v2,v3,v4,v5 = None,None,None,None,None
-    index =-1
-    try : #on s'assure de récuperer les bonnes valeurs
-        #print(v1,v2,v3,v4,v5)
-        v1= float(transversal_permittiviy.get())
-        v2= float(longitudinal_permittiviy.get())
-        v3=float(diameter.get())
-        v4= float(azimuthal_index.get())
+    global view, index, panel, window, transversal_permittiviy, longitudinal_permittiviy, diameter, azimuthal_index, k_max_var,list_te_tm_var
+    v1, v2, v3, v4, v5 = None, None, None, None, None
+    index = -1
+    list_te_tm = []
+    try:  # on s'assure de récuperer les bonnes valeurs
+        # print(v1,v2,v3,v4,v5)
+        v1 = float(transversal_permittiviy.get())
+        v2 = float(longitudinal_permittiviy.get())
+        v3 = float(diameter.get())
+        v4 = int(azimuthal_index.get())
         v5 = float(k_max_var.get())
-        index = int(v1+v2+v3+v4)
-        print(v1,v2,v3,v4,v5)
-        #print("k_max = {}".format(v5))
+        index = int(v1 + v2 + v3 + v4)
+        list_te_tm = list_te_tm_var.get().split(',')
+        print(f' Transerse permittivity {v1},\n Longitudinal permittivity {v2},\n diameter {v3} m,\n azimuthal index {v4},\n frequency {v5} GHz,\n range of modes {list_te_tm}')
+        # print("k_max = {}".format(v5))
     except:
         print("les paramètres sont vides ou format incorrect")
-        print(v1,v2,v3,v4,v5)
-        
-    if index ==-1 :
+        print(v1, v2, v3, v4, v5, list_te_tm)
+
+    if index == -1:
         return
-    
-    view = views[index%len(views)]
-    result = tk.Toplevel(window) 
-    img = ImageTk.PhotoImage(file= view)
-    graph = tk.Label(result, image = img)
-    menubar = tk.Menu(result)  
-    file = tk.Menu(menubar, tearoff=0)  
-    file.add_command(label="Save")   
+    main(v1,v2,v3,v4,v5,index,list_te_tm)
+    view = ".images/graph"+str(index)+".png" #views[index % len(views)]
+    result = tk.Toplevel(window)
+    img = ImageTk.PhotoImage(file=view)
+    graph = tk.Label(result, image=img)
+    menubar = tk.Menu(result)
+    file = tk.Menu(menubar, tearoff=0)
+    file.add_command(label="Save")
 
     file.add_separator()
 
-    file.add_command(label="Exit", command=result.quit)  
-    menubar.add_cascade(label="Fichier", menu=file)  
-    
-    graph.pack()
-    result.config(menu=menubar)  
-    result.mainloop()  
+    file.add_command(label="Exit", command=result.quit)
+    menubar.add_cascade(label="Fichier", menu=file)
 
-if __name__ =="__main__":
-	create_main_window()
+    graph.pack()
+    result.config(menu=menubar)
+    result.mainloop()
+
+
+if __name__ == "__main__":
+    create_main_window()
--- plotter.py
+++ plotter.py
@@ -5,11 +5,24 @@ from scipy import optimize
 import scipy.special as sp
 from functools import partial
 
-c0 = 3e8
-eps_t = 10
-eps_l = 10
-d = 0.016
-n = 1
+c0 = 299792458
+eps_t = None#10
+eps_l = None# 10
+d = None #0.016
+n = None #1
+
+
+def mouse_move(event):
+    x, y = event.xdata, event.ydata
+    print(x, y)
+
+def plot_unit_circle():
+    angs = np.linspace(0, 2 * np.pi, 10**6)
+    rs = np.zeros_like(angs) + 1
+    xs = rs * np.cos(angs)
+    ys = rs * np.sin(angs)
+    plt.plot(xs, ys)
+
 
 
 # Implicit function of the dispersion's equation
@@ -19,13 +32,13 @@ def f(beta, k):
     h_air = np.sqrt(beta ** 2 - k ** 2)
     X = (d / 2) * h_air
     Y_e = (d / 2) * kce
-    Y_m = (d / 2) * kcm
+    #Y_m = (d / 2) * kcm
 
     DTE = ((sp.kvp(n, X, 1)) / (X * sp.kn(n, X))) + ((sp.jvp(n, Y_e, 1)) / (Y_e * sp.jn(n, Y_e)))
     DTM = ((sp.kvp(n, X, 1)) / (X * sp.kn(n, X))) + (eps_l * (sp.jvp(n, Y_e, 1)) / (Y_e * sp.jn(n, Y_e)))
-    DH = (n ** 2) * ((1 / X ** 2) + (1 / Y_e ** 2)) * ((1 / X ** 2) + (eps_l / Y_m ** 2))
+    #DH = (n ** 2) * ((1 / X ** 2) + (1 / Y_e ** 2)) * ((1 / X ** 2) + (eps_l / Y_m ** 2))
 
-    return [DTE, DTM, DH]
+    return [DTE, DTM]#, DH]
 
 
 def dte(beta, k):
@@ -52,31 +65,7 @@ def zero_deepfinder(f, a, b, xtol, Niter):
     return res
 
 
-Nk = 300  # Discretization
-k_min = 10
-k_max = 1000
-
-k = np.linspace(k_min, k_max, Nk)
-
-k = np.delete(k, -1, axis=0)  # The strict inequality implies to remove the last element
-
-xtol = 0.0001
-Beta_te = []
-Beta_tm = []
-#Beta_th=[]
-for k0 in k:    
-    #Niter = int(1/2 * k0 * (np.sqrt(eps_t) - 1))
-    Niter=100
-    z_te = zero_deepfinder(partial(dte, k=k0), k0 * (10001 / 10000), k0 * np.sqrt(eps_t) * (9999 / 10000), xtol, Niter)
-    z_tm = zero_deepfinder(partial(dtm, k=k0), k0 * (10001 / 10000), k0 * np.sqrt(eps_t) * (9999 / 10000), xtol, Niter)
-    #z_th = zero_deepfinder(partial(dth, k=k0), k0 * (10001 / 10000), k0 * np.sqrt(eps_t) * (9999 / 10000), xtol, Niter)
-    Beta_tm.append(z_tm)
-    Beta_te.append(z_te)
-    #Beta_th.append(z_th)
-
-
-
-def X_dte(idx):
+def X_dte(idx,Beta_te):
     x = []
     if len(Beta_te[-1]) < idx + 1:
         raise ValueError("this mode doesn't exist")
@@ -89,12 +78,12 @@ def X_dte(idx):
     return x
 
 
-def Y_dte(idx):
-    x = X_dte(idx)
+def Y_dte(idx,k,Nk,Beta_te):
+    x = X_dte(idx,Beta_te)
     return k[Nk - 1 - len(x)::]
 
 
-def X_dtm(idx):
+def X_dtm(idx,Beta_tm,):
     x = []
     if len(Beta_tm[-1]) < idx + 1:
         raise ValueError("this mode doesn't exist")
@@ -107,31 +96,69 @@ def X_dtm(idx):
     return x
 
 
-def Y_dtm(idx):
-    x = X_dtm(idx)
+def Y_dtm(idx,k,Nk,Beta_tm):
+    x = X_dtm(idx,Beta_tm)
     return k[Nk - 1 - len(x)::]
 
-u=9 # unit Giga
-plt.figure()
-plt.xlabel(r'$\beta$')
-plt.grid(visible=True, linestyle='--')
-plt.ylabel('$f$ [GHz]')
-plt.ylim([0, k_max * (c0/(2*10**u*scipy.pi))])
-plt.xlim([0, k_max * np.sqrt(eps_t)])
-# -- limits domain
-plt.axline((0, 0), slope=(c0/(2*10**u*scipy.pi)), c='red')
-plt.axline((0, 0), slope=(c0/(2*10**u*scipy.pi)) / np.sqrt(eps_t), c='red')
+def main(esp_t_p,esp_l_p,d_p,n_p,f_max,index, list_te_tm):
+    global eps_l,eps_t,d,n
+    eps_t = esp_t_p#10
+    eps_l = esp_l_p# 10
+    d = d_p #0.016
+    n = n_p #1
+    u=9 # unit Giga
+    Nk = 300  # Discretization
+    k_min = 10
+    k_max = (2*scipy.pi*f_max*(10**u))/(299792458)
 
-plt.plot(X_dte(0), (c0/(2*10**u*scipy.pi))*Y_dte(0), label='TE0')
-plt.plot(X_dte(1), (c0/(2*10**u*scipy.pi))*Y_dte(1), label='TE1')
-plt.plot(X_dte(2), (c0/(2*10**u*scipy.pi))*Y_dte(2), label='TE2')
-plt.plot(X_dte(3), (c0/(2*10**u*scipy.pi))*Y_dte(3), label='TE3')
+    k = np.linspace(k_min, k_max, Nk)
 
-plt.plot(X_dtm(0), (c0/(2*10**u*scipy.pi))*Y_dtm(0), label='TM0')
-plt.plot(X_dtm(1), (c0/(2*10**u*scipy.pi))*Y_dtm(1), label='TM1')
-plt.plot(X_dtm(2), (c0/(2*10**u*scipy.pi))*Y_dtm(2), label='TM2')
-plt.plot(X_dtm(3), (c0/(2*10**u*scipy.pi))*Y_dtm(3), label='TM3')
+    k = np.delete(k, -1, axis=0)  # The strict inequality implies to remove the last element
 
-plt.legend()
-plt.savefig("graph.pdf", format="pdf", bbox_inches="tight")
-plt.show()
+    xtol = 0.0001
+    Beta_te = []
+    Beta_tm = []
+    #Beta_th=[]
+    for k0 in k:
+        #Niter = int(1/2 * k0 * (np.sqrt(eps_t) - 1))
+        Niter=100
+        z_te = zero_deepfinder(partial(dte, k=k0), k0 * (10001 / 10000), k0 * np.sqrt(eps_t) * (9999 / 10000), xtol, Niter)
+        z_tm = zero_deepfinder(partial(dtm, k=k0), k0 * (10001 / 10000), k0 * np.sqrt(eps_t) * (9999 / 10000), xtol, Niter)
+        #z_th = zero_deepfinder(partial(dth, k=k0), k0 * (10001 / 10000), k0 * np.sqrt(eps_t) * (9999 / 10000), xtol, Niter)
+        Beta_tm.append(z_tm)
+        Beta_te.append(z_te)
+        #Beta_th.append(z_th)
+
+
+
+    u=9 # unit Giga
+    plt.figure()
+    plt.xlabel(r'$\beta$ [m$^{-1}$]')
+    plt.grid(visible=True, which='both', linestyle='--')
+    plt.ylabel('$f$ [GHz]')
+    #plt.ylim([0, k_max * (c0/(2*10**u*scipy.pi))])
+    plt.ylim([0, k_max*(299792458)/(2*scipy.pi*(10**u))])
+    plt.xlim([0, k_max * np.sqrt(eps_t)])
+    # -- limits domain
+    plt.axline((0, 0), slope=(c0/(2*10**u*scipy.pi)), c='red')
+    plt.axline((0, 0), slope=(c0/(2*10**u*scipy.pi)) / np.sqrt(eps_t), c='red')
+
+    for i in list_te_tm:
+        plt.plot(X_dte(int(i),Beta_te), (299792458/(2*scipy.pi*(10**u)))*Y_dte(int(i),k,Nk,Beta_te), label=f'TE$_{{{int(n)}}}$,$_{{{i}}}$')
+        plt.plot(X_dtm(int(i),Beta_tm), (299792458/(2*scipy.pi*(10**u)))*Y_dtm(int(i),k,Nk,Beta_tm), label=f'TM$_{{{int(n)}}}$,$_{{{i}}}$')
+
+
+    """plt.plot(X_dte(1,Beta_te), (c0/(2*10**u*scipy.pi))*Y_dte(1,k,Nk,Beta_te), label='TE1')
+    plt.plot(X_dte(2,Beta_te), (c0/(2*10**u*scipy.pi))*Y_dte(2,k,Nk,Beta_te), label='TE2')
+    plt.plot(X_dte(3,Beta_te), (c0/(2*10**u*scipy.pi))*Y_dte(3,k,Nk,Beta_te), label='TE3')
+
+    plt.plot(X_dtm(0,Beta_tm), (c0/(2*10**u*scipy.pi))*Y_dtm(0,k,Nk,Beta_tm), label='TM0')
+    plt.plot(X_dtm(1,Beta_tm), (c0/(2*10**u*scipy.pi))*Y_dtm(1,k,Nk,Beta_tm), label='TM1')
+    plt.plot(X_dtm(2,Beta_tm), (c0/(2*10**u*scipy.pi))*Y_dtm(2,k,Nk,Beta_tm), label='TM2')
+    plt.plot(X_dtm(3,Beta_tm), (c0/(2*10**u*scipy.pi))*Y_dtm(3,k,Nk,Beta_tm), label='TM3')"""
+
+    plt.legend()
+    plt.connect('motion_notify_event', mouse_move)
+    plot_unit_circle()
+    plt.savefig(".images/graph"+str(index)+".png", format="png", bbox_inches="tight")
+    #plt.show()
